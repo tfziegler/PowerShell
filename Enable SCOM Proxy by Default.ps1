fdb337abfbb6e022dcaca36ea084118e21aad7b5
Tom Ziegler <37410693+tfziegler@users.noreply.github.com>
2022-01-23 14:34:48 -0500

    Add files via upload

---
+++ Enable SCOM Proxy by Default.ps1
@@ -0,0 +1,16 @@
+﻿##################################################################
+#
+# Title: Create Hyper-V Checkpoints.ps1
+# Author: Tom Ziegler
+# Company: 
+# Last Modified: 1-23-2022
+# Version: 1.0
+#
+# Description - Enable Proxy Agent by Default in SCOM 
+##################################################################
+
+
+add-pssnapin "Microsoft.EnterpriseManagement.OperationsManager.Client";
+new-managementGroupConnection -ConnectionString:localhost;
+set-location "OperationsManagerMonitoring::";
+Set-DefaultSetting -Name HealthService\ProxyingEnabled -Value True
