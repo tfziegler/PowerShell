e4f804129bd0f4b79a8667cc3c4df406300d4c99
Tom Ziegler <37410693+tfziegler@users.noreply.github.com>
2022-01-23 14:33:58 -0500

    Add files via upload

---
+++ Change SCOM RMSe.ps1
@@ -0,0 +1,13 @@
+﻿##################################################################
+#
+# Title: Create Hyper-V Checkpoints.ps1
+# Author: Tom Ziegler
+# Company: 
+# Last Modified: 1-23-2022
+# Version: 1.0
+#
+# Description - Change RMSE Server  
+##################################################################
+
+#Change OMMS2.LAB.AD to the Server name you want to become your new RMSe
+Get-SCOMManagementServer -Name OMMS2.LAB.AD |Set-SCOMRMSEmulator
