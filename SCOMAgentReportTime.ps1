fff5499c4cfa34616cb875f4ff85a89458aa20e4
Tom Ziegler <37410693+tfziegler@users.noreply.github.com>
2023-03-13 12:20:54 -0400

    Add files via upload

---
+++ SCOMAgentReportTime.ps1
@@ -0,0 +1,13 @@
+﻿# Connect to the SCOM management group
+Import-Module OperationsManager
+New-SCOMManagementGroupConnection -ComputerName "OMMS1.LAB.AD"
+
+# Retrieve information about all agents
+$agents = Get-SCOMAgent -Name "test*.LAB.AD"
+
+# Loop through each agent and retrieve the last time it reported in
+foreach ($agent in $agents) {
+    $lastReportTime = $agent.LastModified.ToLocalTime()
+    $output = "$($agent.DisplayName) last reported in at $($lastReportTime)"
+    Write-Output $output | out-file C:\temp\test.txt -Append
+}
